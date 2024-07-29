2fd7fa269ea4b2117832ed7969474fc3be112954
Ivo Nascimento <iannsp@gmail.com>
2024-07-29 16:54:15 -0300

    set Class namespace
    refactor to use gql object instead of gqlscript
    
    add useCache to save the Schema file

--- src/Schema.php
+++ src/Schema.php
@@ -1,15 +1,20 @@
 <?php
+namespace Clientedigital\Pipefy;
+
 use Clientedigital\Pipefy\Graphql\GraphQL;
+
 class Schema
 {
     use GraphQL;
 
     public function build(): void
     {
+        Pipefy::useCache(true);
         $schema = $this->request( 
-            $this->getGQL("schema.build")->script()
+            $this->getGQL("schema.build")
         );
         $this->setCache("schema", $schema);
+        Pipefy::useCache(false);
     }
     public function report(): string
     {
