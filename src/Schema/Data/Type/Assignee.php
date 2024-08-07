656d0aecd398e45b172b5e0ff77117c5cc757fed
Ivo Nascimento <iannsp@gmail.com>
2024-08-07 17:01:50 -0300

    remove comment

--- src/Schema/Data/Type/Assignee.php
+++ src/Schema/Data/Type/Assignee.php
@@ -42,45 +42,4 @@ class Assignee extends AbstractType implements TypeInterface
         return "{fieldId:\"{$this->id()}\", value: \"{$this->value()}\"}";
     }
 }
-/*
-use graphql/org/members
 
-um assignee
-[3]=>
-      object(stdClass)#216 (3) {
-        ["field"]=>
-        object(stdClass)#215 (3) {
-          ["id"]=>
-          string(11) "responsavel"
-          ["type"]=>
-          string(15) "assignee_select"
-          ["internal_id"]=>
-          string(9) "378399061"
-        }
-        ["name"]=>
-        string(11) "responsavel"
-        ["value"]=>
-        string(18) "["Ivo Nascimento"]"
-      }
-    }
-
-
- [3]=>
-      object(stdClass)#216 (3) {
-        ["field"]=>
-        object(stdClass)#215 (3) {
-          ["id"]=>
-          string(11) "responsavel"
-          ["type"]=>
-          string(15) "assignee_select"
-          ["internal_id"]=>
-          string(9) "378399061"
-        }
-        ["name"]=>
-        string(11) "responsavel"
-        ["value"]=>
-        string(36) "["Ivo Nascimento", "Ivo Nascimento"]"
-      }
-    }
-
-*/
