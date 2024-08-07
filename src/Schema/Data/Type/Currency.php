2b6a6d12d15c5cc56c69e7dc3fe05f3245493436
Ivo Nascimento <iannsp@gmail.com>
2024-08-07 11:27:16 -0300

    Currency Pipefy Field Support

--- src/Schema/Data/Type/Currency.php
+++ src/Schema/Data/Type/Currency.php
@@ -1,5 +1,45 @@
-// apresenta sinal da moeda e usa , (deve ter localizacao)
-    public function __construct($value): void;
-    public function script(): string;
-    public function definition(?stdclass $def);
+<?php
+namespace Clientedigital\Pipefy\Schema\Data\Type;
+
+use \stdclass;
+
+class Currency extends AbstractType implements TypeInterface 
+{
+    private ?float $value = null;
+
+    public function value($value = null)
+    {
+        if(!is_null($value)){
+            $this->validate($value);
+            $this->value = number_format($value, 2);
+        }
+
+        if($this->required() && is_null($this->value))
+            throw new \Exception("{$this->id()} is required and cannot be null");
+        
+        return $this->value;
+    }
+
+    private function validate($value)
+    {
+        if(!is_numeric($value) || is_string($value)){
+            $type = gettype($value);
+            throw new \Exception("Currency {$this->id()} only accept Numeric([0-9].{0,2}). Found {$type}.");
+        }
+    }
+
+    /**
+    * use gqlname to select the kind of script you need.
+    * because sometimes a Data Type has a different representation in different gqls
+    **/
+    public function script($gqlname=null): string
+     {
+        $value = $this->value();
+
+        if($gqlname == 'card-create')
+            return "{field_id:\"{$this->id()}\", field_value: \"{$value}\"}";
+
+        return "{fieldId:\"{$this->id()}\", value: \"{$value}\"}";
+    }
+
 }
